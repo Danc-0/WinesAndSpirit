b268af08719a438918a92a8ae2a5001823522d30
Danc-0 <lunjaludaniel@gmail.com>
2020-07-14 11:42:31 +0300

    Final Wines And Spirit Commit

--- app/src/main/java/com/example/databindingdrinks/ChooseQuantityDialog.java
+++ app/src/main/java/com/example/databindingdrinks/ChooseQuantityDialog.java
@@ -11,6 +11,7 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.DialogFragment;
 
+
 import com.example.databindingdrinks.databinding.DialogChooseQuantityBinding;
 
 
